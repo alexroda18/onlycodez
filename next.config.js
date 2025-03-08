fb6c03f514aaecac4d63eb8acf43fd4ed7e6c283
alexroda18 <alexroda1993@gmail.com>
2025-03-08 14:18:23 +0800

    fix code

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  transpilePackages: [
+    '@uiw/react-codemirror',
+    '@uiw/codemirror-extensions-basic-setup',
+    '@codemirror/state',
+    '@codemirror/view',
+    '@codemirror/language',
+    '@codemirror/commands',
+    '@codemirror/theme-one-dark'
+  ],
+  webpack: (config, { isServer }) => {
+    // Fix for CodeMirror ESM/CJS interoperability
+    config.resolve.extensionAlias = {
+      '.js': ['.js', '.ts', '.tsx']
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig 
